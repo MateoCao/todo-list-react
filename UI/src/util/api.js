576ad1fd7c39406d6ea022aea423b066d5abee5b
Mateo Cao <mateocao12@gmail.com>
2023-08-26 12:26:18 -0300

    api url

--- UI/src/util/api.js
+++ UI/src/util/api.js
@@ -1,5 +1,6 @@
 export const API = {
-  url: 'http://localhost:1235/tasks',
+  // url: 'http://localhost:1235/tasks',
+  url: 'https://todo-list-le4v-dev.fl0.io/tasks',
 
   // GET
   async getTasks () {
