097220a96f98e6ad0f10e797a631bb64478b6135
Mateo Cao <mateocao12@gmail.com>
2023-08-30 23:23:05 -0300

    Registro y login, otros cambios

---
+++ API/middlewares/validateToken.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+export const authRequired = (req, res, next) => {
+  const { token } = req.cookies
+  if (!token) return res.status(401).json({ message: 'No token, autorización denegada' })
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token inválido' })
+
+    req.user = user
+    next()
+  })
+}
